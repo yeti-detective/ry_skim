148ee14f331ae4ad8a3b31b06a050aa2dbe693fe
yeti-detective <yeti.detective@gmail.com>
2018-05-24 11:22:00 -0400

    updated javascript

--- bundle.js
+++ bundle.js
@@ -1,2 +1,248 @@
-!function(e){var n={};function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:r})},t.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="",t(t.s=0)}([function(e,n){const t=document.getElementById("canvas").getContext("2d"),r=()=>{t.canvas.width=.9*window.innerWidth,t.canvas.height=.8*window.innerHeight};r(),window.addEventListener("resize",()=>{r()}),(new Image).src="./assets/sonic.png"}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./scripts/script.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./scripts/sanik.js":
+/*!**************************!*\
+  !*** ./scripts/sanik.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Sanik; });
+/* harmony import */ var _sprite__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sprite */ "./scripts/sprite.js");
+
+
+const sanikImg = new Image();
+sanikImg.src = "./assets/sonic.png"
+
+const sonicOptions = {
+  image: sanikImg,
+  sourceX: 0,
+  sourceY: 0,
+  sourceWidth: 30,
+  sourceHeight: 40,
+  destX: 0,
+  destY: 0,
+  destWidth: 30,
+  destHeight: 40
+}
+
+class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
+  constructor(ctx, options = sonicOptions) {
+    super(ctx, options)
+  }
+
+  chill () {
+    this.sourceY = 20;
+    let chillCount = 0;
+    const chillimations = [0, 35, 67, 100, 130]
+    const sonicChill = setInterval(() => {
+      this.unRender();
+      this.sourceX = chillimations[++chillCount % chillimations.length];
+      this.render();
+    }, 500)
+  }
+
+}
+
+
+/***/ }),
+
+/***/ "./scripts/script.js":
+/*!***************************!*\
+  !*** ./scripts/script.js ***!
+  \***************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _sanik__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sanik */ "./scripts/sanik.js");
+// import Sprite from './sprite'
+
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+const canvasSize = () => {
+  ctx.canvas.width = window.innerWidth * 0.9;
+  ctx.canvas.height = window.innerHeight * 0.8;
+}
+
+canvasSize();
+
+window.addEventListener('resize', () => {
+  canvasSize();
+})
+
+// const sanikImg = new Image();
+// sanikImg.src = "./assets/sonic.png";
+//
+// const sanik = new Sprite(ctx, {
+//   image: sanikImg,
+//   sourceX: 0,
+//   sourceY: 0,
+//   sourceWidth: 474,
+//   sourceHeight: 361,
+//   destX: 0,
+//   destY: 0,
+//   destWidth: 474,
+//   destHeight: 361
+// })
+
+const sanik = new _sanik__WEBPACK_IMPORTED_MODULE_0__["default"](ctx)
+
+window.addEventListener('load', () => {
+  
+})
+
+window.sanik = sanik;
+
+
+/***/ }),
+
+/***/ "./scripts/sprite.js":
+/*!***************************!*\
+  !*** ./scripts/sprite.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Sprite; });
+class Sprite {
+  constructor(context, options) {
+    // properties
+    this.context = context;
+    this.img = options.image;
+    this.sourceX = options.sourceX;
+    this.sourceY = options.sourceY;
+    this.sourceWidth = options.sourceWidth;
+    this.sourceHeight = options.sourceHeight;
+    this.destX = options.destX;
+    this.destY = options.destY;
+    this.destWidth = options.destWidth;
+    this.destHeight = options.destHeight;
+
+    // methods
+    this.moveVert = this.moveVert.bind(this);
+    this.moveHoriz = this.moveHoriz.bind(this);
+    this.unRender = this.unRender.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  moveVert(px) {
+    this.unRender();
+    this.destY += px;
+    this.render();
+  }
+
+  moveHoriz(px) {
+    this.unRender();
+    this.destX += px;
+    this.render();
+  }
+
+  unRender() {
+    this.context.clearRect(
+      this.destX,
+      this.destY,
+      this.destWidth,
+      this.destHeight
+    );
+  }
+
+  render () {
+    this.context.drawImage(
+      this.img,
+      this.sourceX,
+      this.sourceY,
+      this.sourceWidth,
+      this.sourceHeight,
+      this.destX,
+      this.destY,
+      this.destWidth,
+      this.destHeight
+    );
+  }
+}
+
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=bundle.js.map

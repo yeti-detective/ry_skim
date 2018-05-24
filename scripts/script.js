99670fcd204ffc18811fb05261441568eb62ff86
yeti-detective <yeti.detective@gmail.com>
2018-05-24 18:10:44 -0400

    all animations handled by single interval

--- scripts/script.js
+++ scripts/script.js
@@ -16,21 +16,6 @@ window.addEventListener('resize', () => {
   canvasSize();
 })
 
-// const sanikImg = new Image();
-// sanikImg.src = "./assets/sonic.png";
-//
-// const sanik = new Sprite(ctx, {
-//   image: sanikImg,
-//   sourceX: 0,
-//   sourceY: 0,
-//   sourceWidth: 474,
-//   sourceHeight: 361,
-//   destX: 0,
-//   destY: 0,
-//   destWidth: 474,
-//   destHeight: 361
-// })
-
 const sanik = new Sanik(ctx)
 let count = 0;
 let sanikActions = [
@@ -39,9 +24,14 @@ let sanikActions = [
   sanik.flip
 ]
 window.addEventListener('load', () => {
+  sanik.chill()
   setInterval(() => {
-    sanikActions[++count % sanikActions.length]();
-  }, 1000)
+    sanik.update();
+    ++count;
+    // if (count % 60 === 0) {
+    //   sanikActions[(count / 60) % sanikActions.length]();
+    // }
+  }, 1000 / 60)
 })
 
 window.sanik = sanik;

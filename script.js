4bc4a38dd56dfcf6d7e4749a95254483126affa6
yeti-detective <yeti.detective@gmail.com>
2018-05-23 23:20:10 -0400

    working hard, so tired

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const testDiv = document.getElementById('testDiv');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
 
-testDiv.innerHTML = '<h3>and javascript is working too, wow</h3>';
+ctx.canvas.width = window.innerWidth * 0.9;
+// TODO: add window resize event listener to make canvas responsive
+
+const sanik = new Image();
+sanik.src = "./assets/sonic.png";
+
+const sprite = options => {
+  const that = {};
+
+  that.context = options.context;
+  that.width = options.width;
+  that.height = options.height;
+  that.image = options.image;
+
+  return that;
+};

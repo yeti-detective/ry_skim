fa59db458535eb12a689f37363bb11350f7c257b
yeti-detective <yeti.detective@gmail.com>
2018-05-25 13:17:16 -0400

    slowed falling speed & added logic to prevent sonic from falling through ground

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,11 +1,14 @@
 const physics = (player, world) => {
   if (player.sprite.destY < 202) {
-    player.vVel += 5;
+    player.vVel += 1;
+  } else if (player.sprite.destY > 202) {
+    player.vVel = 0;
+    player.sprite.destY = 202;
   }
   if (player.hVel > 0) {
-    player.hVel -= 10;
+    player.hVel -= 1;
   } else if (player.hVel < 0) {
-    player.hVel -= 10;
+    player.hVel += 1;
   }
   player.affect();
 }

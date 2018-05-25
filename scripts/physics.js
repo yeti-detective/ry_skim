074c6cacae5d005d26040538ae88680fb0643c34
yeti-detective <yeti.detective@gmail.com>
2018-05-25 15:22:40 -0400

    horizontal inertia no longer necessary

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,14 +1,9 @@
 const physics = (player, world) => {
   if (player.sprite.destY < 202) {
-    player.vVel += 4;
+    player.vVel += 3;
   } else if (player.sprite.destY > 202) {
     player.vVel = 0;
     player.sprite.destY = 202;
-  }
-  if (player.hVel > 0) {
-    player.hVel -= 1;
-  } else if (player.hVel < 0) {
-    player.hVel += 1;
   }
   player.affect();
 }

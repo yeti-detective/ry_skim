ae1ba6bce6d3539174b8947061347ca5136636f7
yeti-detective <yeti.detective@gmail.com>
2018-05-25 09:46:21 -0400

    update bundle

--- bundle.js
+++ bundle.js
@@ -75,38 +75,47 @@
 /*!*******************************!*\
   !*** ./scripts/background.js ***!
   \*******************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Background; });
-/* harmony import */ var _sprite__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sprite */ "./scripts/sprite.js");
+/*! no static exports found */
+/***/ (function(module, exports) {
 
 
-const mushroomKingdom = new Image();
-mushroomKingdom.src = './assets/mushroom_kingdom.png';
+let mushKingdom;
 
-const bgDefaults = {
-  image: mushroomKingdom,
-  sourceX: 0,
-  sourceY: 0,
-  sourceWidth: 900,
-  sourceHeight: 225,
-  destX: 0,
-  destY: 0,
-  destWidth: 900,
-  destHeight: 200
-};
-
-class Background extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
-  constructor(context, options) {
-    const settings = Object.assign({}, bgDefaults, options);
-    super(context, settings);
-  }
-
+const Background = () => {
 
 }
+
+
+
+
+
+
+
+
+//
+// const mushroomKingdom = new Image();
+// mushroomKingdom.src = './assets/mushroom_kingdom.png';
+//
+// const bgDefaults = {
+//   image: mushroomKingdom,
+//   sourceX: 0,
+//   sourceY: 0,
+//   sourceWidth: 900,
+//   sourceHeight: 225,
+//   destX: 0,
+//   destY: 0,
+//   destWidth: 900,
+//   destHeight: 200
+// };
+//
+// export default class Background extends Sprite {
+//   constructor(context, options) {
+//     const settings = Object.assign({}, bgDefaults, options);
+//     super(context, settings);
+//   }
+//
+//
+// }
 
 
 /***/ }),
@@ -143,7 +152,7 @@ class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
   constructor(ctx, options) {
     const settings = Object.assign({}, sonicOptions, options);
     super(ctx, settings);
-
+    this.facingLeft = false;
     this.animArray = [{x: 0, w: 0}];
 
     this.animate = this.animate.bind(this);
@@ -155,7 +164,11 @@ class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
   animate() {
     if (this.animCount % this.speed === 0) {
       if (this.flipped) {
-        this.animArray = this.flipImage(this.animArray);
+        this.facingLeft = !this.facingLeft
+        this.flipped = false;
+      }
+      if (this.facingLeft) {
+        this.animArray = this.leftAnimArr;
       }
       this.unRender();
       this.sourceWidth = this.animArray[(this.animCount / this.speed) % this.animArray.length].w;
@@ -166,7 +179,6 @@ class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
   }
 
   flipImage(arr) {
-    this.flipped = !this.flipped;
     return arr.map((pos) => {
       return {
         x: 984 - (pos.x + pos.w),
@@ -184,6 +196,7 @@ class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
       {x: 100, w: 30},
       {x: 130, w: 30}
     ];
+    this.leftAnimArr = this.flipImage(this.animArray)
     this.speed = 20;
     this.animate();
   }
@@ -199,6 +212,7 @@ class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
       {x: 180, w: 35}
     ];
     this.animArray = walkArr.concat(walkArr.reverse());
+    this.leftAnimArr = this.flipImage(this.animArray)
     this.speed = 10;
     this.animate();
   }
@@ -226,6 +240,7 @@ class Sanik extends _sprite__WEBPACK_IMPORTED_MODULE_0__["default"] {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _background__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./background */ "./scripts/background.js");
+/* harmony import */ var _background__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_background__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _sanik__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./sanik */ "./scripts/sanik.js");
 // import Sprite from './sprite'
 
@@ -244,7 +259,7 @@ canvasSize();
 window.addEventListener('resize', () => {
   canvasSize();
 });
-const background = new _background__WEBPACK_IMPORTED_MODULE_0__["default"](ctx);
+// const background = new Background(ctx);
 const sanik = new _sanik__WEBPACK_IMPORTED_MODULE_1__["default"](ctx);
 let count = 0;
 let sanikActions = [
@@ -261,6 +276,9 @@ window.addEventListener('load', () => {
     ++count;
     if (count % 60 === 0) {
       sanikActions[(count / 60) % sanikActions.length]();
+      if (sanikActions[(count / 60) % sanikActions.length] === undefined) {
+        console.log('whoopsie');
+      }
     }
   }, 1000 / 60);
 });
@@ -320,12 +338,6 @@ class Sprite {
   flip() {
     this.unRender();
     this.flipped = !this.flipped;
-    // this.context.save();
-    // this.context.translate(this.sourceWidth, 0);
-    // this.context.scale(-1, 1);
-    // // this.destX = this.destX * -1;
-    // this.render();
-    // this.context.restore();
   }
 
   unRender() {

2c2e28284959a8a2d6d493c58072f9013a1c9718
yeti-detective <yeti.detective@gmail.com>
2018-05-25 13:12:38 -0400

    cleaning up commented out old code

--- scripts/script.js
+++ scripts/script.js
@@ -15,35 +15,10 @@ canvasSize();
 window.addEventListener('resize', () => {
   canvasSize();
 });
-// const background = new Background(ctx);
-// const sanik = new Sanik(ctx);
-// let count = 0;
-// let sanikActions = [
-//   sanik.chill,
-//   // sanik.flip,
-//   sanik.walk,
-//   // sanik.flip
-// ];
 
 const game = new Game();
 window.addEventListener('load', () => {
-  // sanik.chill();
   setInterval(() => {
     game.tick();
-    // background.update();
-    // sanik.update();
-    // background.unRender();
-    // sanik.unRender();
-    // background.render();
-    // sanik.render();
-    // ++count;
-    // if (count % 60 === 0) {
-    //   sanikActions[(count / 60) % sanikActions.length]();
-    //   if (sanikActions[(count / 60) % sanikActions.length] === undefined) {
-    //     console.log('whoopsie');
-    //   }
-    // }
   }, 1000 / 60);
 });
-
-// window.sanik = sanik;

0899719849f4769d5f9796aecc414d058a315dc3
yeti-detective <yeti.detective@gmail.com>
2018-05-24 20:08:52 -0400

    cycle through sonic actions.

--- scripts/script.js
+++ scripts/script.js
@@ -8,30 +8,31 @@ const ctx = canvas.getContext("2d");
 const canvasSize = () => {
   ctx.canvas.width = window.innerWidth * 0.9;
   ctx.canvas.height = window.innerHeight * 0.8;
-}
+};
 
 canvasSize();
 
 window.addEventListener('resize', () => {
   canvasSize();
-})
+});
 
-const sanik = new Sanik(ctx)
+const sanik = new Sanik(ctx);
 let count = 0;
 let sanikActions = [
   sanik.chill,
+  sanik.flip,
   sanik.walk,
   sanik.flip
-]
+];
 window.addEventListener('load', () => {
-  sanik.chill()
+  sanik.chill();
   setInterval(() => {
     sanik.update();
     ++count;
-    // if (count % 60 === 0) {
-    //   sanikActions[(count / 60) % sanikActions.length]();
-    // }
-  }, 1000 / 60)
-})
+    if (count % 60 === 0) {
+      sanikActions[(count / 60) % sanikActions.length]();
+    }
+  }, 1000 / 60);
+});
 
 window.sanik = sanik;

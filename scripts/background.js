bfc8b26d0c888d296bfefc7d597a362ec7d4281a
yeti-detective <yeti.detective@gmail.com>
2018-05-25 09:29:00 -0400

    none of this is acceptable

--- scripts/background.js
+++ scripts/background.js
@@ -1,25 +1,38 @@
-import Sprite from './sprite';
 
-const mushroomKingdom = new Image();
-mushroomKingdom.src = './assets/mushroom_kingdom.png';
+let mushKingdom;
 
-const bgDefaults = {
-  image: mushroomKingdom,
-  sourceX: 0,
-  sourceY: 0,
-  sourceWidth: 900,
-  sourceHeight: 225,
-  destX: 0,
-  destY: 0,
-  destWidth: 900,
-  destHeight: 200
-};
-
-export default class Background extends Sprite {
-  constructor(context, options) {
-    const settings = Object.assign({}, bgDefaults, options);
-    super(context, settings);
-  }
-
+const Background = () => {
 
 }
+
+
+
+
+
+
+
+
+//
+// const mushroomKingdom = new Image();
+// mushroomKingdom.src = './assets/mushroom_kingdom.png';
+//
+// const bgDefaults = {
+//   image: mushroomKingdom,
+//   sourceX: 0,
+//   sourceY: 0,
+//   sourceWidth: 900,
+//   sourceHeight: 225,
+//   destX: 0,
+//   destY: 0,
+//   destWidth: 900,
+//   destHeight: 200
+// };
+//
+// export default class Background extends Sprite {
+//   constructor(context, options) {
+//     const settings = Object.assign({}, bgDefaults, options);
+//     super(context, settings);
+//   }
+//
+//
+// }

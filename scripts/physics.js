0658a3b55c2e7d902a55c3ba7ddbf3684a3131fb
yeti-detective <yeti.detective@gmail.com>
2018-05-28 11:20:05 -0400

    fixed falling through ground problem. Still don't have multi-tier platforms...

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,9 +1,12 @@
 const physics = (player, world) => {
   if (player.sprite.destY < world.ground) {
-    player.vVel += 3;
-  } else if (player.sprite.destY > world.ground) {
-    player.vVel = 0;
-    player.sprite.destY = world.ground;
+    if (player.sprite.destY + (player.vVel + 3) >= world.ground) {
+      debugger
+      player.vVel = 0;
+      player.sprite.destY = world.ground;
+    } else {
+      player.vVel += 3;
+    }
   }
   player.affect();
 };

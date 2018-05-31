363e60968d1bd68bcb906872ccfee6ca18e414e9
yeti-detective <yeti.detective@gmail.com>
2018-05-30 23:18:46 -0400

    check for right and left barriers

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,4 +1,5 @@
 const physics = (player, world) => {
+  // gravity and ground
   if (player.sprite.destY < world.ground) {
     if (player.sprite.destY + (player.vVel + 3) >= world.ground) {
       player.vVel = 0;
@@ -8,6 +9,12 @@ const physics = (player, world) => {
     }
   } else if (player.sprite.destY > world.ground) {
     player.vVel += 1;
+  }
+  // right and left barriers
+  if (player.sprite.destX <= world.left) {
+    player.sprite.destX += 7;
+  } else if (player.sprite.destX >= world.right) {
+    player.sprite.destX -= 7;
   }
   player.affect();
 };

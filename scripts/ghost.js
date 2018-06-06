2c671750cfd93a757e045ed52b6c0fc2b4608100
yeti-detective <yeti.detective@gmail.com>
2018-06-06 10:56:39 -0400

    ghost class ready to test

--- scripts/ghost.js
+++ scripts/ghost.js
@@ -1,4 +1,36 @@
 import Sprite from './sprite';
 
-const inky = new Image();
-inky.src = './assets/inky_pinky_blinky_clyde.png';
+const ghostImg = new Image();
+ghostImg.src = './assets/inky_pinky_blinky_clyde.png';
+
+export const blinky = {
+  image: ghostImg,
+  sourceX: 2,
+  sourceY: 2,
+  sourceWidth: 14,
+  sourceHeight: 14,
+  destX: 5,
+  destY: 5,
+  destWidth: 20,
+  destHeight: 20
+};
+
+export const pinky = Object.assign({}, blinky, {sourceY: 18});
+export const inky = Object.assign({}, blinky, {sourceY: 34});
+export const clyde = Object.assign({}, blinky, {sourceY: 50});
+
+export default class Ghost extends Sprite {
+  constructor(ctx, options) {
+    super(ctx, options);
+    this.animArray = [2, 17, 34, 50, 66, 82, 97, 113];
+
+    this.animate = this.animate.bind(this);
+  }
+
+  animate () {
+    if (this.animCount % 20 === 0) {
+      const animIndex = (this.animCount / 20) % this.animArray.length;
+      this.sourceY = animArray[animIndex];
+    }
+  }
+}
